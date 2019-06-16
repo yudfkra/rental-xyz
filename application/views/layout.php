96d0d30acc7439a902ecb5c9e644cf08c28b62e2
Yudafikra Andika <yudafikraandika@gmail.com>
2019-06-16 22:33:16 +0700

    fix error on empty tittle

--- application/views/layout.php
+++ application/views/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?php echo ($title ? $title . ' - ' : '') . 'Rental Mobil XYZ' ?></title>
+    <title><?php echo (isset($title) ? $title . ' - ' : '') . 'Rental Mobil XYZ' ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/jumbotron-narrow.css') ?>">
